19a493e3a1ba40d3eb53c8d6882256112c2c6bb8
IanFonzie <fberg_i@hotmail.com>
2021-12-10 16:12:25 -0800

    Complete exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 
-const incrementor = (callback, val) => () => {
-  callback(val + 1);
-}
-
 const Button = ({handleClick, btnText}) => {
   return <button onClick={handleClick}>{btnText}</button>
 }
 
 const Counter = ({label, value}) => {
-  return <p>{label}: {value}</p>
+  return <div>{label}: {value}</div>
+}
+
+const RatioDisplay = ({label, count, total}) => {
+  return <div>{label}: {count / total}</div>
+}
+
+const AvgDisplay = ({label, count, total}) => {
+  return <div>{label}: {count / total * 100} %</div>
 }
 
 const App = () => {
@@ -17,17 +21,37 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [avgCount, setAvgCount] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const statUpdater = (callback, val, modifier) => () => {
+    callback(val + 1)
+    setAvgCount(avgCount + modifier)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={incrementor(setGood, good)} btnText={"good"} />
-      <Button handleClick={incrementor(setNeutral, neutral)} btnText={"neutral"} />
-      <Button handleClick={incrementor(setBad, bad)} btnText={"bad"} />
-      <h2>statistics</h2>
-      <Counter label={"good"} value={good}></Counter>
-      <Counter label={"neutral"} value={neutral}></Counter>
-      <Counter label={"bad"} value={bad}></Counter>
+      <Button 
+        handleClick={statUpdater(setGood, good, 1)} 
+        btnText={"good"} 
+      />
+      <Button
+        handleClick={statUpdater(setNeutral, neutral, 0)}
+        btnText={"neutral"}
+      />
+      <Button
+        handleClick={statUpdater(setBad, bad, -1)}
+        btnText={"bad"}
+      />
+      <h1>statistics</h1>
+      <Counter label={"good"} value={good} />
+      <Counter label={"neutral"} value={neutral} />
+      <Counter label={"bad"} value={bad} />
+      <Counter label={"total"} value={total} />
+      <RatioDisplay label={"average"} count={avgCount} total={total} />
+      <AvgDisplay label={"positive"} count={good} total={total} />
     </div>
   )
 }
