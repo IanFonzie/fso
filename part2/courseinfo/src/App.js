07a5abbb32f494257504211da9163e4c94cc2548
IanFonzie <fberg_i@hotmail.com>
2021-12-14 00:25:34 -0800

    Complete exercise 2.2 & 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -7,9 +7,14 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts.reduce((exerciseCount, part) => {
+    return exerciseCount + part.exercises
+  }, 0)
+
   return(
-    <p>Number of exercises {sum}</p>
+    <p>
+      <b>total of {sum} exercises</b>
+    </p>
   ) 
 }
 
@@ -60,7 +65,12 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
-      }
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      },
     ]
   }
 
