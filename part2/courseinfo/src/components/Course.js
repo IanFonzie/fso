a3f8e23a5a6bf0f9043c2af7be961f254faa2f8d
IanFonzie <fberg_i@hotmail.com>
2021-12-14 00:52:27 -0800

    Complete exercise 2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,47 @@
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((exerciseCount, part) => {
+    return exerciseCount + part.exercises
+  }, 0)
+
+  return(
+    <p>
+      <b>total of {sum} exercises</b>
+    </p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => {
+        return <Part key={part.id} part={part} />
+      })}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default Course
