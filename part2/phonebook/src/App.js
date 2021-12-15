6a8214cd247293a8c9a2c947a6ee2bbe65d60ba2
IanFonzie <fberg_i@hotmail.com>
2021-12-14 20:06:51 -0800

    Complete exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleChange = (setState) => event => setState(event.target.value)
 
@@ -23,9 +27,17 @@ const App = () => {
     alert(`${newName} is already added to the phonebook`)
   }
 
+  const toDisplay = filter ?
+    persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())) :
+    persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleChange(setFilter)}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleChange(setNewName)} />
@@ -38,7 +50,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {toDisplay.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
