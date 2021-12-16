167f76a397aff4acdce8bbcb3fc73005b88fd49a
IanFonzie <fberg_i@hotmail.com>
2021-12-15 19:53:51 -0800

    Complete exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,21 +1,61 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const directions = ['N','NNE','NE','ENE','E','ESE','SE','SSE','S','SSW','SW','WSW','W','WNW','NW','NNW']
+const degreesToCompass = degrees => {
+  const sanitizedDegrees = degrees % 360
+  return directions[Math.round(sanitizedDegrees / 22.5) % 16]
+}
+
+const fahrenheitToCelsius = (degreesF) => {
+  return Math.round((degreesF - 32) * (5/9))
+}
+
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState(undefined)
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?` + 
+           `q=${country.capital[0]},${country.cca2}&` +
+           `appid=${apiKey}&` +
+           `units=imperial`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [])
+
   const languages = []
   for (let key in country.languages) {
     languages.push(<li key={key}>{country.languages[key]}</li>)
   }
+  
+  let weatherDetails
+  if (weather) {
+    weatherDetails = (
+      <>
+        <div><b>temperature:</b> {fahrenheitToCelsius(weather.main.temp)} Celsius</div>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
+        <div>
+          <b>wind:</b> {Math.round(weather.wind.speed)} mph direction {degreesToCompass(weather.wind.deg)}
+        </div>
+      </>
+    )
+  }
+  
   return (
     <div>
       <h1>{country.name.common}</h1>
-      <div>capital {country.capital}</div>
+      <div>capital {country.capital[0]}</div>
       <div>population {country.population}</div>
-      <h2>languages</h2>
+      <h2>Spoken languages</h2>
       <ul>
         {languages}
       </ul>
       <img src={country.flags.png} />
+      <h2>Weather in {country.capital[0]}</h2>
+      {weatherDetails}
     </div>
   )
 }
